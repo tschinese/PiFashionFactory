1f355b6615373230ef3f54ea75e7cc5f0f83402a
tschinese <marcel.riedl.mr@googlemail.com>
2016-01-08 17:23:25 +0100

    Marcel Riedl 08.01.2016 17:22 Änderungen: Backend_Neu.css, einbindung von css im backend

--- app/view/Groese/Groese_View.php
+++ app/view/Groese/Groese_View.php
@@ -8,72 +8,74 @@ Beschreibung: Hier wird der View für die Groese erstellt. -->
 
 <!-- Sprint 5 Marcel Riedl Start -->
 <main>
-    <br><br><br>
+
     <!-- Überschrift -->
     <div>
-        <h4> Größen bearbeiten </h4>
+        <h1> Größen bearbeiten </h1>
     </div>
-    <div>
-        <!-- Tabelle für die vorhandenen Daten aus der Datenbank erstellen-->
-        <table>
-            <!-- Tabellen Kopf erstellen -->
-            <thead>
-                <tr>Vorhandene Größen</tr>
-            </thead>
-            <!-- Anzeigen der Daten aus der Datenbank-->
-            <?php
-            // Counter erstellen
-            $a = 0;
-            // Größe des Arrays ermitteln
-            $total = sizeof($data);
-            // Tabellen Body erstellen
-            echo '<tbody>';
-            // Für jeden Datensatz wird eine neue Zeile erstellt
-            while ($a < $total) {
-                // Ausgabe der Daten:
-                echo '<tr><td>' . $data[$a]['groese'] . '</td></tr>';
-                // Counter erhöhen
-                $a++;
-            }
-            echo '</tbody>';
-            ?>
-        </table>
-    </div>
-    <!-- Formular um eine Größe zu löschen -->
-    <div>
-        <form method="post">
-            <label for="groese">zu löschende Größe:</label> 
-            <select name="groese">
+    <div id="divAbstand">
+        <div>
+            <!-- Tabelle für die vorhandenen Daten aus der Datenbank erstellen-->
+            <table class="table table-striped">
+                <!-- Tabellen Kopf erstellen -->
+                <thead>
+                    <tr><th>Vorhandene Größen</th></tr>
+                </thead>
+                <!-- Anzeigen der Daten aus der Datenbank-->
                 <?php
-                // neuer Counter
-                $b = 0;
-                // Abfrage aller Auswahlmöglichkeiten
-                while ($b < $total) {
-                    // Ausgabe der Möglichkeiten
-                    echo '<option>' . $data[$b]['groese'] . '</option>';
-                    // Erhöhung des Counters
-                    $b++;
+                // Counter erstellen
+                $a = 0;
+                // Größe des Arrays ermitteln
+                $total = sizeof($data);
+                // Tabellen Body erstellen
+                echo '<tbody>';
+                // Für jeden Datensatz wird eine neue Zeile erstellt
+                while ($a < $total) {
+                    // Ausgabe der Daten:
+                    echo '<tr><td>' . $data[$a]['groese'] . '</td></tr>';
+                    // Counter erhöhen
+                    $a++;
                 }
+                echo '</tbody>';
                 ?>
-            </select><br>
-            <!-- Bestätigungsbutton -->
-            <input type="submit" name="loeschen" value="Löschen"><br>
-        </form>
-    </div>
-    <!-- Formular um eine Größe hinzuzufügen -->
-    <div>
-        <form action="index.php?url=GroeseController" method="post">
-            <!-- Eingabe der neuen Größe -->
-            <label for="hinzugroese">hinzuzufügende Größe: </label><input name="hinzugroese" type="text"><br>
-            <!-- Bestätigungsbutton -->
-            <input type="submit" name="hinzufuegen" value="Hinzufügen"><br>
-        </form>
-    </div>
-    <!-- Formular um zurück zum Hauptmenu des Backends zukommen -->
-    <div>
-        <form action="index.php?url=backend" method="post">
-            <input type="submit" value="Zurück">
-        </form>
+            </table>
+        </div>
+        <!-- Formular um eine Größe zu löschen -->
+        <div>
+            <form method="post">
+                <label for="groese">zu löschende Größe:</label> 
+                <select name="groese">
+                    <?php
+                    // neuer Counter
+                    $b = 0;
+                    // Abfrage aller Auswahlmöglichkeiten
+                    while ($b < $total) {
+                        // Ausgabe der Möglichkeiten
+                        echo '<option>' . $data[$b]['groese'] . '</option>';
+                        // Erhöhung des Counters
+                        $b++;
+                    }
+                    ?>
+                </select><br>
+                <!-- Bestätigungsbutton -->
+                <input type="submit" name="loeschen" value="Löschen"><br>
+            </form>
+        </div>
+        <!-- Formular um eine Größe hinzuzufügen -->
+        <div>
+            <form action="index.php?url=GroeseController" method="post">
+                <!-- Eingabe der neuen Größe -->
+                <label for="hinzugroese">hinzuzufügende Größe: </label><input name="hinzugroese" type="text"><br>
+                <!-- Bestätigungsbutton -->
+                <input type="submit" name="hinzufuegen" value="Hinzufügen"><br>
+            </form>
+        </div>
+        <!-- Formular um zurück zum Hauptmenu des Backends zukommen -->
+        <div>
+            <form action="index.php?url=backend" method="post">
+                <input type="submit" value="Zurück">
+            </form>
+        </div>
     </div>
 </main>
 
